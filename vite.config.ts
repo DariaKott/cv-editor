2279b67351c02734266732ff99f47126f45b2b3e
Daria Kot <poturayko@gmail.com>
2025-06-28 17:48:46 +0200

    Set up deploy with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/cv-editor/', // имя репозитория
   plugins: [react()],
-    resolve: {
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
