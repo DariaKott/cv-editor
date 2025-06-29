02e24c6113f153a74daf695a9c39f711eeff5e80
Daria Kot <poturayko@gmail.com>
2025-06-29 11:27:06 +0200

    Added pdf-loader

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,3 +1,6 @@
+import { useRef } from "react";
+import html2pdf from "html2pdf.js";
+
 import type { ResumeSection } from "@/types/resume";
 import type { ProfileData } from "../ProfileForm/ProfileForm";
 import { Button } from "@/shared/ui/Button/Button";
@@ -15,28 +18,45 @@ type Props = {
 };
 
 export const Preview = ({ profile, sections }: Props) => {
+  const resumeRef = useRef<HTMLDivElement>(null);
+
+  const handleDownloadPDF = () => {
+    if (!resumeRef.current) return;
+
+    html2pdf()
+      .from(resumeRef.current)
+      .set({
+        margin: 0.5,
+        filename: "resume.pdf",
+        image: { type: "jpeg", quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+      })
+      .save();
+  };
+
   return (
-    <div className={styles.preview}>
-      <ProfilePreview data={profile} />
+    <div className={styles.wrapper}>
+      <div ref={resumeRef} className={styles.preview}>
+        <ProfilePreview data={profile} />
 
-      {sections.map((section) => {
-        switch (section.type) {
-          case "education":
-            return <EducationPreview key={section.id} data={section.data} />;
-          case "experience":
-            return <ExperiencePreview key={section.id} data={section.data} />;
-          case "about":
-            return <AboutPreview key={section.id} data={section.data} />;
-          case "skills":
-            return <SkillsPreview key={section.id} data={section.data} />;
-          default:
-            return null;
-        }
-      })}
+        {sections.map((section) => {
+          switch (section.type) {
+            case "education":
+              return <EducationPreview key={section.id} data={section.data} />;
+            case "experience":
+              return <ExperiencePreview key={section.id} data={section.data} />;
+            case "about":
+              return <AboutPreview key={section.id} data={section.data} />;
+            case "skills":
+              return <SkillsPreview key={section.id} data={section.data} />;
+            default:
+              return null;
+          }
+        })}
+      </div>
 
-      <Button onClick={() => console.log("PDF отправлен")}>
-        Отправить PDF
-      </Button>
+      <Button onClick={handleDownloadPDF}>Скачать как PDF</Button>
     </div>
   );
 };
